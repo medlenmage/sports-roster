3a576413638b21e58d8e02fa305a72446652e30f
Joshua Medlen <joshua246@gmail.com>
2020-08-20 17:40:43 -0500

    setting up auth but erroring when it tries to sign in

---
+++ src/components/Auth/Auth.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+class Auth extends React.Component {
+  loginClickEvent = (e) => {
+    e.preventDefault();
+    const googleProvider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(googleProvider);
+  }
+
+  render() {
+    return (
+      <div className="Auth">
+        <button className="btn btn-primary" onClick={this.loginClickEvent}><i className="fas fa-baseball-ball">Play Ball!!!</i></button>
+      </div>
+    );
+  }
+}
+
+export default Auth;
