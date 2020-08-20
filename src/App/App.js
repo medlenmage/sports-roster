137699da7ebb23ed9312748ec49c6afd82930305
Joshua Medlen <joshua246@gmail.com>
2020-08-20 17:53:51 -0500

    working on auth. fixed my dummy mistakes

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+// import fbConnection from '../helpers/data/connections';
 import './App.scss';
 import Auth from '../components/Auth/Auth';
 
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    // const { authed } = this.state;
     return (
       <div className="App">
         <h2>Atlanta Braves</h2>
