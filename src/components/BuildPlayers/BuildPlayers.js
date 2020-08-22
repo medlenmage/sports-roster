7988dd56f9c7a0259edfcfb871fc2d1e19bf06b6
Joshua Medlen <joshua246@gmail.com>
2020-08-22 13:05:24 -0500

    added screenshot for read me and future inclusion of bio site. resized the cards and player pictures

--- src/components/BuildPlayers/BuildPlayers.js
+++ src/components/BuildPlayers/BuildPlayers.js
@@ -19,7 +19,7 @@ class BuildPlayer extends React.Component {
 
     return (
       <div className="card">
-        <img className="card-img-top" src={player.imgUrl} alt={player.name}></img>
+        <img className="card-img-top player-img" src={player.imgUrl} alt={player.name}></img>
         <div className="card-body">
           <h5 className="card-title">{player.name}</h5>
           <p className="card-text">{player.name} plays {player.position} for the Atlanta Braves</p>
